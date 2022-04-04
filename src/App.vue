e4db12abd14caf862530aad30eef287b2b755a2c
Ozioma <oziomaokoroafor@gmail.com>
2022-04-04 20:27:42 +0100

    Edit module uri

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import LandingPage from '/./src/components/main/page/LandingPage.vue';
+import LandingPage from './components/main/page/LandingPage.vue';
 
 export default {
   name: 'App',
